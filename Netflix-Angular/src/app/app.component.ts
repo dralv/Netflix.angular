2e87ce78c411adb5362fe2d13c5e24e46fbd1caa
Alvaro <dralvphd2@gmail.com>
2022-08-06 16:48:02 -0300

    Adicionando imagens

--- Netflix-Angular/src/app/app.component.ts
+++ Netflix-Angular/src/app/app.component.ts
@@ -19,17 +19,17 @@ export class AppComponent {
     categories: ['Épico', 'Filme de fantasia','Viagem no tempo']
   },
   {
-    trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-    cover:'assets/images/guardios-da-galaxia.jpg',
-    name:'Guardiões da Galaxia 2',
+    trailer:'assets/videos/capitao_america_guerra_civil_1080p.mp4',
+    cover:'assets/images/capitao-america-o-primeiro-vingador.jpg',
+    name:'Capitão América',
     relevance:80,
     age:12,
     parts: 2,
-    categories: ['Épico', 'Filme de fantasia','Viagem no tempo']
+    categories: ['Épico', 'Filme de fantasia','Ação']
   },
   {
     trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-    cover:'assets/images/guardios-da-galaxia.jpg',
+    cover:'assets/images/homem-de-ferro-2.jpg',
     name:'Guardiões da Galaxia 3',
     relevance:55,
     age:18,
@@ -38,7 +38,7 @@ export class AppComponent {
   },
   {
     trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-    cover:'assets/images/guardios-da-galaxia.jpg',
+    cover:'assets/images/eu-sou-a-lenda.jpg',
     name:'Guardiões da Galaxia 3',
     relevance:55,
     age:18,
